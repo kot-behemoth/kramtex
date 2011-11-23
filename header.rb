57b7c66e1621c40f799c2d7af8741c4e05c426bb
Gregory Goltsov <gregory@goltsov.info>
2011-11-23 18:45:04 +0000

    Actually works now. Very unpolished.

--- header.rb
+++ header.rb
@@ -1,43 +1,51 @@
 class LaTeXheaders
   
   attr_reader :header, :end
+  attr_accessor :opts
 
   def initialize(_opts={})
-    opts = {:title => 'Title Here',
+    @opts = {:title => 'Title Here',
      :type => 'article', 
      :font_weight => '10pt',
-     :author => 'KramTeX',
-     :roman_font => 'Hoefler Text',
+     :author => '',
+     :font => 'Hoefler Text',
      :sans_font => 'Gill Sans',
      :mono_font => 'Andale Mono',
      :date => ''}.merge!(_opts)
+  end
 
+  def generate
   @header = 
 %Q{%!TEX TS-program = xelatex
 %!TEX encoding = UTF-8 Unicode
 
-\\documentclass[#{opts[:font_weight]}]{#{opts[:type]}}
+\\documentclass[#{@opts[:font_weight]}]{#{@opts[:type]}}
 \\usepackage{geometry}         % See geometry.pdf to learn the layout options. There are lots.
 \\geometry{letterpaper}        % ... or a4paper or a5paper or ... 
 %\\geometry{landscape}         % Activate for for rotated page geometry
 \\usepackage[parfill]{parskip} % Activate to begin paragraphs with an empty line rather than an indent
 \\usepackage{graphicx}
-\\usepackage{amssymb}
 \\usepackage{hyperref}
 \\usepackage{wrapfig}
+\\usepackage{longtable}
+\\usepackage{amsmath}
+\\usepackage{amssymb}
+\\usepackage{color}
+\\usepackage{fancyvrb}
 
 \\usepackage{fontspec,xltxtra,xunicode}
 \\defaultfontfeatures{Mapping=tex-text}
-\\setromanfont[Mapping=tex-text]{#{opts[:roman_font]}}
-\\setsansfont[Scale=MatchLowercase,Mapping=tex-text]{#{opts[:sans_font]}}
-\\setmonofont[Scale=MatchLowercase]{#{opts[:mono_font]}}
+\\setromanfont[Mapping=tex-text]{#{@opts[:font]}}
+\\setsansfont[Scale=MatchLowercase,Mapping=tex-text]{#{@opts[:sans_font]}}
+\\setmonofont[Scale=MatchLowercase]{#{@opts[:mono_font]}}
 
-\\title{#{opts[:title]}}
-\\author{#{opts[:author]}}
-\\date{#{opts[:date]}}
+\\title{#{@opts[:title]}}
+\\author{#{@opts[:author]}}
+\\date{#{@opts[:date]}}
 
 \\begin{document}
-\\maketitle }
+\\maketitle
+\\#{@opts[:pagestyle_empty]}\pagestyle{empty}}
 
   @end = "\n\\end{document}"
   end
